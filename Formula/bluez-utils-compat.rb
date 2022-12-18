f4f5c1926ec8b8b596647d024208ed5378969e85
Jessica Stokes <hello@jessicastokes.net>
2022-12-18 12:52:45 -0800

    bluez-utils-compat: Fix formula syntax

--- Formula/bluez-utils-compat.rb
+++ Formula/bluez-utils-compat.rb
@@ -1,37 +1,35 @@
 class BluezUtilsCompat < Formula
-  desc "Development and debugging utilities for the bluetooth protocol stack. Includes deprecated tools."
+  desc "Development and debugging utilities for the bluetooth protocol stack"
   homepage "http://www.bluez.org/"
   url "http://www.kernel.org/pub/linux/bluetooth/bluez-5.66.tar.xz"
   sha256 "39fea64b590c9492984a0c27a89fc203e1cdc74866086efb8f4698677ab2b574"
   license "GPL2"
 
-  depends_on :linux
   depends_on "docutils" => :build
+  # NOTE: This is to work around Homebrew docutils formula bug homebrew/homebrew-core#118458
+  depends_on "python@3.11" => :build
   depends_on "alsa-lib"
   depends_on "dbus"
-  depends_on "python@3.11" => :build
   depends_on "glib"
   depends_on "libical"
+  depends_on :linux
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
     system "./configure",
-	       *std_configure_args,
-		   "--disable-silent-rules",
-		   "--enable-midi",
-		   "--enable-sixaxis",
-		   "--enable-hid2hci",
-		   "--enable-mesh",
-		   "--enable-experimental",
-		   "--enable-library",
-		   "--enable-deprecated"
+           *std_configure_args,
+           "--disable-silent-rules",
+           "--enable-midi",
+           "--enable-sixaxis",
+           "--enable-hid2hci",
+           "--enable-mesh",
+           "--enable-experimental",
+           "--enable-library",
+           "--enable-deprecated"
     system "make"
-	system "make",
-	       "install-binPROGRAMS",
-		   "install-dist_zshcompletionDATA",
-		   "install-man1"
+    system "make",
+           "install-binPROGRAMS",
+           "install-dist_zshcompletionDATA",
+           "install-man1"
   end
 
   test do
