4bca0fcb95b0a19be544ea82bfadac5b3eced000
Jessica Stokes <hello@jessicastokes.net>
2023-10-28 17:04:07 -0700

    linuxconsole: Fix license

--- Formula/linuxconsole.rb
+++ Formula/linuxconsole.rb
@@ -3,7 +3,7 @@ class Linuxconsole < Formula
   homepage "https://sourceforge.net/projects/linuxconsole/"
   url "https://prdownloads.sourceforge.net/linuxconsole/linuxconsoletools-1.8.1.tar.bz2"
   sha256 "4da29745c782b7db18f5f37c49e77bf163121dd3761e2fc7636fa0cbf35c2456"
-  license "GPL"
+  license "GPL-1.0-or-later"
 
   depends_on :linux
   depends_on "sdl2"
