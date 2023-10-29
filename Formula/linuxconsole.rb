96db9bfcc6e69ea70201074bf55ccd908e586f09
Jessica Stokes <hello@jessicastokes.net>
2023-10-28 17:17:44 -0700

    linuxconsole: fix download URL

--- Formula/linuxconsole.rb
+++ Formula/linuxconsole.rb
@@ -1,12 +1,13 @@
 class Linuxconsole < Formula
   desc "Set of utilities for joysticks and serial devices"
   homepage "https://sourceforge.net/projects/linuxconsole/"
-  url "https://prdownloads.sourceforge.net/linuxconsole/linuxconsoletools-1.8.1.tar.bz2"
+  url "https://downloads.sourceforge.net/project/linuxconsole/linuxconsoletools-1.8.1.tar.bz2"
   sha256 "4da29745c782b7db18f5f37c49e77bf163121dd3761e2fc7636fa0cbf35c2456"
   license "GPL-1.0-or-later"
 
   depends_on :linux
   depends_on "sdl2"
+  depends_on "systemd"
 
   def install
     system "make", "SYSTEMD_SUPPORT=1"
